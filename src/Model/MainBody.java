daff80da0cc494b297d37766ddda1816fdcbe94f
Jedrzej Hodor <jedrzej.hodor@student.uj.edu.pl>
2018-02-28 20:55:50 +0100

    Add plan, what to do

---
+++ src/Model/MainBody.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class MainBody {
+    // wybiera usera, pobiera jego baze danych
+    // tworzy learning dla tego usera w wybranym przez niego jezyku i odpala
+    // zapisuje zmiany w bazie danych usera, tzn utrwala jego zestaw
+
+    // poza tym trzeba jakies proste wpisywanie nowych slow do ogolnej bazy danych,
+    // oraz automatyczne importowanie do baz danych userow ze stanem 0.
+    // gdzies trzeba wsadzic opcje usuwania przez usera slowa ze swojej bazy danych, zeby nie miec smieci (tylko w stanie 0)
+
+
+}
